97441628a4f8cff0ef6db8aec1974c7d68cf5d45
danielluccio <daniel130877@gmail.com>
2025-06-25 13:56:29 -0300

    Refatoração da classe com Principio da Responsabilidade Unica

--- class/SistemaCadastro.py
+++ class/SistemaCadastro.py
@@ -1,17 +1,26 @@
 import time
 
 class SistemaCadastro:
+#SINGLE_RESPOMSABILITY
 
-
-    def cadastro(self, nome, idade):
-        if isinstance(nome, str) and (idade, int):
-            print("Acessando o banco de dados...")
-            time.sleep(3)
-            print(f"Nome: {nome}\nIdade: {idade}")      
-
+    def cadastro(self, nome: str, idade: int):
+        if self.__validate_input(nome, idade):
+            self.__register_user(nome, idade)
         else:
-            print("Dados Inválidos")  
+            self.__error_handle()
+
+    
+    def __validate_input(self, nome:str, idade: int):
+        return isinstance(nome, str) and isinstance(idade, int)
+    
+    def __register_user(self, nome:str, idade:int) -> None:
+        time.sleep(3)
+        print("Acessando o Banco de Dados")
+        print(f"Nome: {nome}\nIdade: {idade}")  
+
+    def __error_handle(self) -> None:
+        print("Dados Inválidos")
 
 
-user_1 = SistemaCadastro()
-user_1.cadastro("Daniel Lucio", 19)
+
+
